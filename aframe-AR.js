b76cdb15f3a00d9c8cbd0bc5db41a5b553d019ab
Jash <bangdiwala.jash.16@gmail.com>
2017-06-29 17:09:49 -0700

    CORS https

--- aframe-AR.js
+++ aframe-AR.js
@@ -1273,7 +1273,7 @@ THREEx.ArToolkitProfile.prototype.mypadMarker = function () {
 	this.contextParameters.detectionMode = 'mono'
 
 	this.defaultMarkerParameters.type = 'pattern'
-	this.defaultMarkerParameters.patternUrl = 'http://cors.io/?u=https://pastebin.com/raw/AYKn4DmJ'
+	this.defaultMarkerParameters.patternUrl = 'https://cors.io/?u=https://pastebin.com/raw/AYKn4DmJ'
 	return this
 }
 
@@ -1881,7 +1881,7 @@ AFRAME.registerComponent('artoolkitmarker', {
 			this.data.patternUrl = THREEx.ArToolkitContext.baseURL+'../data/data/patt.kanji'
 		}else if( this.data.preset === 'mypad' ){
 			this.data.type = 'pattern'
-			this.data.patternUrl = 'http://cors.io/?u=https://pastebin.com/raw/AYKn4DmJ'
+			this.data.patternUrl = 'https://cors.io/?u=https://pastebin.com/raw/AYKn4DmJ'
 		}
 
 
